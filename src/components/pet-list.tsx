c8386cbbf6ce7ea5897f2a0dfc273110943bdff4
Daniel Popp <dpopp783@gmail.com>
2025-04-22 22:21:00 -0600

    extract reused border style

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -7,7 +7,7 @@ export default function PetList() {
   const { pets, activePetId, handleChangeActivePetId } = usePetContext();
 
   return (
-    <ul className="bg-white border-b border-black/[0.08]">
+    <ul className="bg-white border-b border-light">
       {pets.map((pet) => (
         <li key={pet.id}>
           <button
